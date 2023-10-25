49ac28667a4437be5a02b5d60558619218ad50a2
can inan <cninnworks@gmail.com>
2023-10-25 21:48:54 +0300

    end

--- src/App.js
+++ src/App.js
@@ -3,29 +3,41 @@ import "./Tailwind.css";
 import Card from "./Components/Card";
 import Navbar from "./Components/Navbar";
 
-
 class App extends React.Component {
   state = {
     movies: [],
-    findMovie: "", 
+    findMovie: "",
   };
-  
 
-  async componentDidMount(){
+  async componentDidMount() {
     //!tmdb my movie list
-    const response = await fetch(`https://api.themoviedb.org/3/list/8275714?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/list/8275714?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+    );
     const data = await response.json();
-    console.log(data.items);
 
-    this.setState({movies:data.items});
-    
-
+    this.setState({ movies: data.items });
   }
 
+  deleteMovie = async (movie) => {
 
 
-  deleteMovie = (movie) => {
+    const url = `https://api.themoviedb.org/3/list/8275714/remove_item?media_id=${movie.id}&api_key=${process.env.REACT_APP_API_KEY}&language=en-US`;
+
+    const options = {
+      method: "POST",
+      headers: {
+     
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+      },
+    };
+    // fetch isteği yapılıyor
+    await fetch(url, options);
+  
+
+    // fetch işlemi tamamlandığında json verileri burada kullanılabilir
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
     this.setState((state) => ({
       movies: newMovieList,
     }));
@@ -40,26 +52,22 @@ class App extends React.Component {
     const findMovie = this.state.findMovie.toLowerCase(); // State içindeki arama metnini küçük harfe çevir
 
     let filteredMovies = this.state.movies.filter((f) => {
-      
       return f.title.toLowerCase().indexOf(findMovie) !== -1; // Her ikisini de küçük harfle karşılaştır
     });
 
     return (
       <main className="app">
-
-
-          {filteredMovies.length > 0 ? null : (
-              //?css ile uygun şekilde özelleştirebilirsiniz.
-            <h1 className="filter-warning">Aradığınız kriterlere uygun film bulunamadı</h1>
-          )}
-      
-
+        {filteredMovies.length > 0 ? null : (
+          //?css ile uygun şekilde özelleştirebilirsiniz.
+          <h1 className="filter-warning">
+            Aradığınız kriterlere uygun film bulunamadı
+          </h1>
+        )}
 
         <Navbar searchMovieProp={this.searchMovie} />
 
         <div className="flex bg-[#ffd60a] ">
           <Card movies={filteredMovies} deleteProps={this.deleteMovie} />
-       
         </div>
       </main>
     );
